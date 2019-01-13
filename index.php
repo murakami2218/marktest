3e89ae9bd1262e419543a3849ffacc043e60c35b
murakami88 <murakami2218@gmail.com>
2019-01-13 21:22:36 +0900

    成績画面の編集

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
     <li><a href="No1_ready.php">平方完成No.1</a></li>
     <li><a href="No2_ready.php">平方完成No.2</a></li>
     <li><a href="No3_ready.php">平方完成No.3</a></li>
-    <li><a href="No4_ready.php">平方完成No.4</a></li>
-    <li><a href="No5_ready.php">平方完成No.5</a></li>
 </ul>
 </body>
 </html>
--- result.php
+++ result.php
@@ -10,18 +10,51 @@
 <?php
 for ($i=1; $i<17; $i++) {
     ${"value_".$i} = $_POST[$i];
-    var_dump(${"value_".$i});
+    //var_dump(${"value_".$i});
 }
-$total = 0;
 // 問１の採点
 if ($value_1 == 2 && $value_2 == 5) {
-    $total += 20;
+    $ans_1 = 20;
+} else {
+    $ans_1 = 0;
 }
 // 問２の採点
 if ($value_3 == 1 && $value_4 == 1) {
-    $total += 20;
+    $ans_2 = 20;
+} else {
+    $ans_2 = 0;
 }
-echo $total;
+// 問３の採点
+if ($value_5 == 3 && $value_6 == 4 && $value_7 == 2 && $value_8 == 3 && $value_9 == 8) {
+    $ans_3 = 20;
+} else {
+    $ans_3 = 0;
+}
+// 問４の採点
+if ($value_10 == 1 && $value_11 == 2 && $value_12 == 7 && $value_13 == 4) {
+    $ans_4 = 20;
+} else {
+    $ans_4 = 0;
+}
+// 問５の採点
+if ($value_14 == 1 && $value_15 == 3 && $value_16 == 4) {
+    $ans_5 = 20;
+} else {
+    $ans_5 = 0;
+}
+$total = 0;
+for ($i=1; $i<6; $i++) {
+    $total += ${"ans_".$i};
+    if (${"ans_".$i} == 0) {
+        $temp = str_replace(" ", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", str_pad(${"ans_".$i}, 2, " ", STR_PAD_LEFT));
+        echo "問{$i} $temp\n点"."<br>";
+    } else {
+        echo "問{$i} &nbsp;&nbsp;&nbsp;${"ans_".$i}\n点"."<br>";
+    }
+}
+$total = str_replace(" ", "&nbsp;&nbsp;", str_pad($total, 3, " ", STR_PAD_LEFT));
+echo "<br>";
+echo "合計 {$total}&nbsp;点";
 ?>
 </body>
 </html>
